87a60c511c888b45c413286a6799b2d0b628fcf9
Saurav1501 <saurav.amca@gmail.com>
2019-04-15 10:11:15 +0530

    LEED City Fix

---
+++ AutomationArc/Object Repository/Manage/CityCom/AddData.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AddData</name>
+   <tag></tag>
+   <elementGuidId>1ea799d5-cf07-4d36-95a3-4b93415fdfea</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[(text() = 'Additional Data' or . = 'Additional data')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
