6f5eb6b51a0b69c0b0f90d9f87e0ddf9531d126a
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-04-15 11:08:27 +0530

    Comm None

--- AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
@@ -57,7 +57,7 @@
             <profileName>STG-US</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/CommunitySuites/USComSuite/USComNoneSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
@@ -165,7 +165,7 @@
             <profileName>STG-US</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/CitiesSuites/USCitySuite/USLEEDForCitiesSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
