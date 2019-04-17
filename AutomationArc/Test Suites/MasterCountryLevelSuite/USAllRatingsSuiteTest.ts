caa43d38610ba208742d96de76d0da8dd70f865c
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-04-17 12:02:19 +0530

    Building trial

--- AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
@@ -111,7 +111,7 @@
             <profileName>STG-US</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/TrialFullAccess1To5YearsSuite/USBuildingTrialFullAccess1To5YearsSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
@@ -138,7 +138,7 @@
             <profileName>STG-US</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/PortfolioSuite/PortfolioSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
