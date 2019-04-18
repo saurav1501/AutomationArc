f8f99264759e897dbad6a0bedea18f60973a815d
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-04-18 23:33:18 +0530

    City LEED

---
+++ AutomationArc/Object Repository/PreRequisite/AffirmationTextPrerequisite.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>AffirmationTextPrerequisite</name>
+   <tag></tag>
+   <elementGuidId>319d76ee-bead-4c02-b686-bab7fae02815</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[text()='Affirmation required before changing the status']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
