f35e5ce0aaa9e6b8e0ff4c320830960fe9bc6cf9
Saurav1501 <saurav.amca@gmail.com>
2019-04-22 15:15:11 +0530

    adding suite

--- AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
@@ -165,7 +165,7 @@
             <profileName>QAS-US</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/CitiesSuites/USCitySuite/USLEEDForCitiesSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
    </testSuiteRunConfigurations>
