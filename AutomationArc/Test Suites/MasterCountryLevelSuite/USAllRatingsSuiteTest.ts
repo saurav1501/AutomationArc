d4134880fe427df999081ae23bce65e09f06a074
Saurav1501 <saurav.amca@gmail.com>
2019-04-25 17:28:41 +0530

    pull

--- AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/USAllRatingsSuiteTest.ts
@@ -12,7 +12,7 @@
             <profileName>QAS-US</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/Transit/Aboveground/USTransitSuite/USTransitAbovegroundSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
