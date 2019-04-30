055c54a9e88e75bd0ae60fea1a04230bf0488eb4
Saurav1501 <saurav.amca@gmail.com>
2019-04-30 15:36:21 +0530

    Parking changes

--- AutomationArc/Keywords/com/arc/ReusableMethods/ReusableMethodsAddNewProject.groovy
+++ AutomationArc/Keywords/com/arc/ReusableMethods/ReusableMethodsAddNewProject.groovy
@@ -269,15 +269,12 @@ public class ReusableMethodsAddNewProject extends BaseClass{
 		 WebUI.delay(5)*/
 	}
 
-
-
-
 	@Keyword
 	public void parkingAddNewProject(String sheetName , int rowNum) {
 		String prjName      = data.getCellData(sheetName,"ProjectName", rowNum)
 		String prjType 		= data.getCellData(sheetName, "ProjectType", rowNum)
 		String prjRating 	= data.getCellData(sheetName, "RatingSystem", rowNum)
-		String spaceType        = data.getCellData(sheetName, "SpaceType", rowNum)
+		String spaceType      = data.getCellData(sheetName, "SpaceType", rowNum)
 		String ownerType 	= data.getCellData(sheetName, "OwnerType", rowNum)
 		String ownerOrg 	= data.getCellData(sheetName, "OwnerOrganization", rowNum)
 		String ownerCountry = data.getCellData(sheetName, "OwnerCountry", rowNum)
@@ -290,6 +287,8 @@ public class ReusableMethodsAddNewProject extends BaseClass{
 		String prjCountry 	= data.getCellData(sheetName, "Country", rowNum)
 		String prjState 	= data.getCellData(sheetName, "State", rowNum)
 		String prjZip 		= data.getCellData(sheetName, "Zip", rowNum)
+		String area 		= data.getCellData(sheetName, "Area", rowNum)
+		
 		Date date = new Date(System.currentTimeMillis())
 
 		String proName
@@ -306,22 +305,23 @@ public class ReusableMethodsAddNewProject extends BaseClass{
 			proName="CNParking"
 		}
 
-		String ProjectName = proName +prjRating +formatarDate.format(date)
+		String ProjectName = proName +formatarDate.format(date)
 		data.setCellData(sheetName,"ProjectName", rowNum, ProjectName)
 
 		navigation.clickAddProject()
 		WebUI.sendKeys(findTestObject('Object Repository/AddProjectNewUI/projectName'), ProjectName)
-		WebUI.selectOptionByLabel(findTestObject('Object Repository/AddProjectNewUI/selectProjectType'), prjType, true)
+		WebUI.delay(1)
+		WebUI.selectOptionByLabel(findTestObject('Object Repository/AddProjectNewUI/selectProjectType'),prjRating,false )
 		WebUI.setText(findTestObject('Object Repository/AddProjectNewUI/noOfParkingSpaces'),parkingSpaces )
 		WebUI.setText(findTestObject('Object Repository/AddProjectNewUI/noOfParkingLevel'),parkingStructure)
-		WebUI.click(findTestObject('Object Repository/AddProjectNewUI/spaceType'))
-		List<WebElement> list= driver.findElements(By.xpath("//*[@ng-repeat='type in spaceType']"))
+		//WebUI.click(findTestObject('Object Repository/AddProjectNewUI/spaceType'))
+		/*List<WebElement> list= driver.findElements(By.xpath("//*[@ng-repeat='type in spaceType']"))
 		int size = list.size()
 		int randonNumber = ThreadLocalRandom.current().nextInt(0, size)
 		println list.get(randonNumber).getText()
 		data.setCellData(sheetName, "SpaceType", rowNum,list.get(randonNumber).getText())
 		list.get(randonNumber).click()
-		WebUI.click(findTestObject('Object Repository/AddProjectNewUI/ownerType'))
+		WebUI.click(findTestObject('Object Repository/AddProjectNewUI/ownerType'))*/
 		List<WebElement> list1= driver.findElements(By.xpath("//*[@ng-repeat='type in ownerType']"))
 		int size1 = list1.size()
 		int randonNumber1 = ThreadLocalRandom.current().nextInt(0, size1)
@@ -330,14 +330,20 @@ public class ReusableMethodsAddNewProject extends BaseClass{
 		data.setCellData(sheetName, "OwnerType", rowNum,list1.get(randonNumber1).getText())
 		/*WebUI.selectOptionByLabel(findTestObject('Object Repository/AddProjectNewUI/spaceType'), spaceType, true)
 		 WebUI.selectOptionByLabel(findTestObject('Object Repository/AddProjectNewUI/ownerType'), ownerType, true)*/
+		
 		WebUI.setText(findTestObject('Object Repository/AddProjectNewUI/organization'),ownerOrg)
 		WebUI.click(findTestObject('Object Repository/AddProjectNewUI/organization'))
 		WebUI.delay(2)
-		WebUI.click(findTestObject('Add_Project_Details/span_Habitat for Humanity'))
+		WebUI.click(findTestObject('Add_Project_Details/Humidity'))
 		WebUI.sendKeys(findTestObject('Object Repository/AddProjectNewUI/ownerEmail'), ownerMail)
 		WebUI.selectOptionByLabel(findTestObject('Object Repository/AddProjectNewUI/ownerCountry'), ownerCountry, false)
+		
+		WebUI.sendKeys(findTestObject('Add_Project_Details/Area'), area)
+		WebUI.delay(1)
+		
 		WebUI.sendKeys(findTestObject('Object Repository/AddProjectNewUI/streetName'), prjAddress)
 		WebUI.delay(2)
+		
 		WebUI.setText(findTestObject('Object Repository/AddProjectNewUI/cityName'), prjCity)
 		WebUI.selectOptionByLabel(findTestObject('Object Repository/AddProjectNewUI/countryName'), prjCountry, false)
 		WebUI.selectOptionByLabel(findTestObject('Object Repository/AddProjectNewUI/stateName'),prjState, false)
@@ -345,7 +351,7 @@ public class ReusableMethodsAddNewProject extends BaseClass{
 		WebUI.click(findTestObject('Object Repository/AddProjectNewUI/clickOnSignAgreement'))
 		WebUI.click(findTestObject('Object Repository/AddProjectNewUI/addProjectNextButton'))
 		WebUI.delay(5)
-		WebUI.waitForElementVisible(findTestObject('Object Repository/paymentPageNewUI/paymentPageTextPurchase'),20)
+		WebUI.waitForElementVisible(findTestObject('Object Repository/paymentPageNewUI/paymentPageTextPurchase'),40)
 		String purchase= WebUI.getText(findTestObject('Object Repository/paymentPageNewUI/paymentPageTextPurchase'))
 		WebUI.verifyMatch("Purchase", purchase, false)
 
