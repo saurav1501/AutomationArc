7dbde52b36de0a4aa28fd7820089fd1d89ed2771
Saurav1501 <saurav.amca@gmail.com>
2019-05-08 16:46:46 +0530

    Build changes

--- AutomationArc/Test Suites/CitiesSuites/USCitySuite/USCitiesNoneSuite.ts
+++ AutomationArc/Test Suites/CitiesSuites/USCitySuite/USCitiesNoneSuite.ts
@@ -13,7 +13,7 @@
    <testCaseLink>
       <guid>93a0f938-cb26-49da-aede-bc416eacf015</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/LoginWithCitesTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -55,62 +55,61 @@
    <testCaseLink>
       <guid>5a3490df-0cca-4e01-9a54-fd504c01989a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/AgreementFileDownloadTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>e42ba676-ccc5-4766-9c85-1612bcc24e3f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Common/CityComValidation/UIValidationPercentageEDUCATIONPopulationWithHighSchoolHumanExperienceTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3999c084-de70-46ba-a9a0-43633a464e44</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Common/CityComValidation/UIValidationPopulationWith(at least)BachelorDegreeHumanExperienceTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>49239a95-d2e8-4f05-a1ce-a0272c1c0030</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Common/CityComValidation/UIValidationPercentageEQUITABILITYHumanExperienceTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>1666e115-d39e-4cf4-a81a-4630f6d6e877</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Common/CityComValidation/UIValidationGiniCoefficient(for income distribution)HumanExperienceTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2c77d49d-43c8-4942-ae4d-cb7465232cf8</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/ScoreVersionVerificationTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>1185dba2-0d28-4886-aa5a-600eccf05198</guid>
+      <guid>24d0c582-df8a-40c0-80cf-f63799612c4e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/NavigateToAdminToolAddStarCertification_1_15_4Test</testCaseId>
    </testCaseLink>
-<<<<<<< HEAD
    <testCaseLink>
       <guid>7a4b9107-d4e8-451e-9691-a66198383683</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/VerifyAddedStarCertificationInsideProject_1_15_4Test</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3c7cba85-6481-4b64-99f3-b85ea0f7b759</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/LoginWithCitesTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>e930f3da-a0d6-404c-a612-895d6c207b93</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/AddNewProjectTest</testCaseId>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -134,37 +133,37 @@
    <testCaseLink>
       <guid>7bb0e74b-a6f4-442d-8d21-e8f548b235ae</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/RegPaymentByCreditCardTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>1d3d90b4-520b-43a7-abb2-940e04d2f3d0</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/SearchProgramTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>67d66939-eeea-4601-8eeb-cf83173f0c75</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/AgreementFileDownloadTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>04d63b4b-f2f4-4b8a-9f51-f973d0276a3b</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/ScoreVersionVerificationTest</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>d5865881-69b3-4dbd-87a2-e7637e43fb8d</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/NavigateToAdminToolAddStarCertification_1_15_4Test</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>b5e1ff94-91af-4247-ac7c-7c8ee6e060d1</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/MyCities/CitiesNone/VerifyAddedStarCertificationInsideProject_1_15_4Test</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -179,6 +178,4 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Common/1_15_4/NoneOtherToolTipText_1_15_4Test</testCaseId>
    </testCaseLink>
-=======
->>>>>>> 4d499c204a4d0d000cfeb3dac2c3792378136df8
 </TestSuiteEntity>
