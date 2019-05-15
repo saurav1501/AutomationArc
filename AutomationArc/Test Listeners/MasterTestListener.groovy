db404dd9b120651023a6cb52807ba2e4e0c3510e
Saurav1501 <saurav.amca@gmail.com>
2019-05-15 10:06:57 +0530

    Master Listener changes

--- AutomationArc/Test Listeners/MasterTestListener.groovy
+++ AutomationArc/Test Listeners/MasterTestListener.groovy
@@ -68,7 +68,7 @@ public class MasterTestListener extends BaseClass {
 		println Country
 		//WebUI.openBrowser('')
 		
-		CustomKeywords.'com.arc.ReusableMethods.ReusableMethodsLogin.loginIntoArcWithGlobalVariable'()
+		//CustomKeywords.'com.arc.ReusableMethods.ReusableMethodsLogin.loginIntoArcWithGlobalVariable'()
 
 		CustomKeywords.'com.arc.ReusableMethods.ReusableMethodsLogin.loginIntoArcWithGlobalVariableAppUrl'()
 
