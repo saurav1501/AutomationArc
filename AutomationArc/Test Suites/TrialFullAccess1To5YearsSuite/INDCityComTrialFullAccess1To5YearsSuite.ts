37306ad7c9a6d7e0141bcb61667b1410087871c1
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-05-16 13:39:43 +0530

    city trials

---
+++ AutomationArc/Test Suites/TrialFullAccess1To5YearsSuite/INDCityComTrialFullAccess1To5YearsSuite.ts
@@ -0,0 +1,181 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>INDCityComTrialFullAccess1To5YearsSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4e361e8b-7b78-4884-a544-cd95075c466d</testSuiteGuid>
+   <testCaseLink>
+      <guid>4e0340fc-dcb6-41de-8741-bcc746797f38</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/AddNewProjectTest</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>eccbff4c-1fc5-4cf2-baeb-1df326b51ef9</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>af679a78-4a46-4d03-9faa-1744b708e570</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>baae5fdd-09cf-4e51-87f3-362a5455414b</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2346d54a-a411-49d2-8519-06e4f328b0e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/CreateEnergyMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>96a9c0e3-e616-48da-b3da-b806f6e6d794</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/VerifyActivityCommentCityCommDoesnotIncludeMeterName_1_15_4Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c424d7fd-8f30-4497-9df4-b7d5014166e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/CreateWaterMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d30520e8-e429-4245-92b4-7605a47d2b7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/CreateWasteGenerationMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>54888514-bd32-4a26-bb94-84c6e81f4ab7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/CreateTrasportMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d6172f50-b24d-46b1-8e05-0c4a659137b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/CreateHumanExpMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0efd79ab-9102-4c0d-94f5-7d3e88b44976</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/DashboardNavigationEndToEndTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b84f3144-6d02-4897-b427-5091d9aa274b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/TrialProjectDetailsVerificationTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3b3abd60-b4bc-41f5-b231-7401ed63753b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/TrialProjectScoreVerificationTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9fac6639-e632-4ebf-b08e-95d9ff46c4e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/5INDRegPaymentByCheckTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>df8aea75-cde0-4408-9c72-9a187e26ba3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/VerifyThePayNowPopUpAfterPaymentTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cfd2d845-08ac-4e5e-8a21-320e02d00fe2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/MyCity/5YearTrailFullAccessCity/5YearBillingAmountAndStatusTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9bab4c18-93fe-4792-a9a4-34537171be8e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/AddNewProjectTest</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>fe6103ae-cb2e-4f5c-af6c-520fc39ca003</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bfd2d141-2061-49aa-adc3-b77ba3a1672f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b2292166-7381-4789-87b0-e50bd0eb4617</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cce2522a-1f96-4d86-b789-1e3d384b5796</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/CreateEnergyMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>265c33af-c90a-4c60-8c26-8b249f779850</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/CreateWaterMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ec170c84-1f80-4961-84ef-31b094f45f47</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/CreateWasteGenerationMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00f81bc9-e624-4823-b801-77b2f4bca921</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/CreateTrasportMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a7ace757-a3c0-47b2-ad98-4a09fe2d67af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/CreateHumanExpMeterReadingTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ee27ecd5-cba6-4449-80ed-2b39cd33191a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/DashboardNavigationEndToEndTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00d51286-2c6b-468d-9f80-16c1a98434b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/TrialProjectDetailsVerificationTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bb74d843-6df1-44e0-8e80-0c9e5f08c822</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TrialAndFullAccessProj1To5Year/Community/TrialProjectScoreVerificationTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
