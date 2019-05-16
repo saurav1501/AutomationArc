54fa6c909423ae8467b07bf2d20de86232ac56d4
Saurav1501 <saurav.amca@gmail.com>
2019-05-16 12:22:47 +0530

    origin master

--- AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection1.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection1.ts
@@ -57,7 +57,7 @@
             <profileName>STG-US-P1</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/BuildingOther/USBuildingOtherSuite/USBuildingOtherSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
