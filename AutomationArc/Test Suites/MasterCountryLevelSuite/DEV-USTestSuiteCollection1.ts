7c0909203df6fa64c90817723bacafec72e6f8cd
Saurav1501 <saurav.amca@gmail.com>
2019-05-17 10:21:32 +0530

    dev build

--- AutomationArc/Test Suites/MasterCountryLevelSuite/DEV-USTestSuiteCollection1.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/DEV-USTestSuiteCollection1.ts
@@ -30,7 +30,7 @@
             <profileName>DEV-US-P1</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/PortfolioSuite/PortfolioSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
