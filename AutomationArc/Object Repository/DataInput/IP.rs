2483e35f2865eb49fcd21ba43683b6881ce397ad
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-05-28 13:51:36 +0530

    arc test suites modified

---
+++ AutomationArc/Object Repository/DataInput/IP.rs
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Unit type IP</description>
+   <name>IP</name>
+   <tag></tag>
+   <elementGuidId>c1b90fc1-4389-49fa-b2d0-8b515414eb30</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@ng-click=&quot;selectValue('IP', ['project'], ['unitType'])&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/DataInput/iframe_datainput-widget</value>
+   </webElementProperties>
+</WebElementEntity>
