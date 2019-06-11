cc57c73bd45607f2136cd71532145998fbc0de5a
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-06-11 18:45:35 +0530

    Building Other

--- AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection1.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection1.ts
@@ -66,7 +66,7 @@
             <profileName>STG-US-P1</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/PortfolioSuite/GRESBSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
