7e9a6e00c8802e7c43d21c0a47c3fd128d2d9891
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-06-17 15:50:38 +0530

    Excel template issues fixed

--- AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection1.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection1.ts
@@ -30,7 +30,7 @@
             <profileName>STG-US-P1</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/MySchools/MySchools</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
@@ -48,7 +48,7 @@
             <profileName>STG-US-P2</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>true</runEnabled>
+         <runEnabled>false</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/BuildingOther/USBuildingOtherSuite/USBuildingOtherSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
