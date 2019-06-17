3f9cb34182d7f103a9c7c24d458435d1499682b1
Abhishek-Digha <abhishek.digha1990@gmail.com>
2019-06-17 11:59:02 +0530

    Local Changes

--- AutomationArc/Scripts/MyCities/CitiesNone/AddNewProjectTest/Script1530082262682.groovy
+++ AutomationArc/Scripts/MyCities/CitiesNone/AddNewProjectTest/Script1530082262682.groovy
@@ -1,14 +1,17 @@
 import java.util.concurrent.ThreadLocalRandom
 
+import com.kms.katalon.core.util.KeywordUtil
+
 import internal.GlobalVariable as GlobalVariable
 
 try {
 	
-	   def rowNumArray = [6,7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+	   def rowNumArray = [6,7, 8,9,10,11,12,13,14,15,16,17,18,19,20]
 	   int rowNumSi = rowNumArray.size()
 	   int rowSize = ThreadLocalRandom.current().nextInt(0, rowNumSi)
 	   int rowNum = rowSize + 5 
 	   println rowNum
+	   KeywordUtil.logInfo("Random Value"+rowNum)
 	   
 	 //  CustomKeywords.'com.arc.ReusableMethods.ReusableMethodsNavigation.navigateToCities'()
 	   CustomKeywords.'com.arc.ReusableMethods.ReusableMethodsAddNewProject.addNewProjectCityScore'(GlobalVariable.CitySheet,GlobalVariable.rowNumFour=rowNum)
