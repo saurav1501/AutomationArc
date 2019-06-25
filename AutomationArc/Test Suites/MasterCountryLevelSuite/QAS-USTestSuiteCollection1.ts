6cdc33070576d49051dbff739528fe862a987c38
Abhishek-Digha <abgupta@usgbc.org>
2019-06-25 09:52:42 +0530

    Saurav pul mrged

--- AutomationArc/Test Suites/MasterCountryLevelSuite/QAS-USTestSuiteCollection1.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/QAS-USTestSuiteCollection1.ts
@@ -45,7 +45,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>QAS-US-P1</profileName>
+            <profileName>QAS-US-P2</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
