87a8f150c484bbf71b02357065fa3578f9a992c6
abhi <abhishek.digha1990@gmail.com>
2019-07-19 11:40:55 +0530

    USAll Rating system

--- AutomationArc/Object Repository/Manage/BillingSection/Measurabl/MonthlyPaymentTextLabel.rs
+++ AutomationArc/Object Repository/Manage/BillingSection/Measurabl/MonthlyPaymentTextLabel.rs
@@ -7,11 +7,10 @@
    <selectorCollection>
       <entry>
          <key>BASIC</key>
-         <value></value>
       </entry>
       <entry>
          <key>XPATH</key>
-         <value>//*[@class=&quot;checkcontainer ng-binding&quot;]</value>
+         <value>//*[@class=&quot;checkcontainer&quot;]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
