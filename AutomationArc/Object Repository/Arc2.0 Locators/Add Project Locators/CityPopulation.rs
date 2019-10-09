a7ebb8024f2388bf5b46d2ee9f1c9ee34fb68798
abhi <abhishek.digha1990@gmail.com>
2019-10-09 16:50:52 +0530

    arc-2.0 locators

---
+++ AutomationArc/Object Repository/Arc2.0 Locators/Add Project Locators/CityPopulation.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Population field for the city projects.</description>
+   <name>CityPopulation</name>
+   <tag></tag>
+   <elementGuidId>633fb28e-3b3f-44c6-81d8-8a13188ba6b5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@name=&quot;occupancy&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ AutomationArc/Object Repository/Arc2.0 Locators/Add Project Locators/TransitStationType.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>Transit project station type, whether Aboveground or Underground.</description>
+   <name>TransitStationType</name>
+   <tag></tag>
+   <elementGuidId>b7d2ffe5-0a9d-4309-a3ef-bde01c227d2a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@ng-model=&quot;formdata.station_type&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
