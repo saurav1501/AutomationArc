7bed9a0bec50a5dcb1f4c3cafe79de2bd8817e8c
Saurav1501 <saurav.amca@gmail.com>
2019-11-05 15:44:22 +0530

    Fixes

--- AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection2.ts
+++ AutomationArc/Test Suites/MasterCountryLevelSuite/STG-USTestSuiteCollection2.ts
@@ -12,7 +12,7 @@
             <profileName>STG-US-P2</profileName>
             <runConfigurationId>Firefox</runConfigurationId>
          </configuration>
-         <runEnabled>false</runEnabled>
+         <runEnabled>true</runEnabled>
          <testSuiteEntity>Test Suites/BuildingSuites/Transit/Aboveground/USTransitSuite/USTransitAbovegroundSuite</testSuiteEntity>
       </TestSuiteRunConfiguration>
       <TestSuiteRunConfiguration>
